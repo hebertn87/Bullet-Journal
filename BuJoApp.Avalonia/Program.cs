b648d254caed746a917d2da5ad2151d53c06808d
Nate Hebert <hebertn87@gmail.com>
2018-12-08 23:42:23 -0700

    Pushed up new version that contains all cross platform versions

---
+++ BuJoApp.Avalonia/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+using BuJoApp.Avalonia.ViewModels;
+using BuJoApp.Avalonia.Views;
+
+namespace BuJoApp.Avalonia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuildAvaloniaApp().Start<MainWindow>(() => new MainWindowViewModel());
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .UseReactiveUI()
+                .LogToDebug();
+    }
+}
