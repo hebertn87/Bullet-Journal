a5073bd8cd0a2291ff0e69e1f88d676ad6df0eb2
Nate Hebert <hebertn87@gmail.com>
2018-12-12 06:07:54 -0700

    pushed up tests?

---
+++ BuJoApp.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void AddTask()
+        {
+            
+        }
+
+        [Test]
+        public void DelTask()
+        {
+            
+        }
+    }
+}
